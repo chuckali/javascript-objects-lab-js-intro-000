a6d3f05c532f08ab5690cb50906d1c4ba5d024d6
Charles Cusson <charlescusson@gmail.com>
2020-03-29 21:44:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ var newObj = Object.assign({},object, {[key] : value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  //var newObj = Object.assign({},object)
 Object.assign(object, {[key] : value})
     return object
 }
-
+function deleteFromObjectByKey(object, key){
+  var newObj = object
+  delete newObj.key
+}
 
 /*
-    destructivelyUpdateObjectWithKeyAndValue(object, key, value)
-      4) updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object
     deleteFromObjectByKey(object, key)
       5) deletes `key` from a clone of object and returns the new object (it is non-destructive)
       6) does not modify the original object (it is non-destructive)
